04216e288a1f7e63ca2ea5ad9f5d23a1fe6763a1
Fuwad13 <princefuwad@gmail.com>
2025-02-13 18:31:21 +0600

    fixed segtree

--- Code snippets/segment tree.cpp
+++ Code snippets/segment tree.cpp
@@ -11,10 +11,9 @@ struct segment_tree{
     void build(const vector<T>& arr, int node, int b, int e){
         if(b==e){
             stree[node] = arr[b];
-/**
- * @brief 
- * 
- */>>1;
+            return;
+        }
+        int mid = (b+e)>>1;
         build(arr, 2*node, b, mid);
         build(arr, 2*node+1, mid+1, e);
         stree[node] = merge(stree[2*node],stree[2*node+1]);
@@ -35,6 +34,7 @@ struct segment_tree{
         set(2*node+1, mid+1, e, ind, val);
         stree[node] = merge(stree[2*node],stree[2*node+1]);
     
+    }
     void set(int ind, T val){set(1, 0, n-1, ind, val);}
 
     void update(int node, int b, int e, int ind, T val){
